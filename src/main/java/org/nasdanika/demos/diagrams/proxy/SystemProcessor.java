087ed85dbee0676f0468491bb228953c5de11e24
Pavel <Pavel.Vlasov@nasdanika.org>
2024-09-14 17:01:17 -0400

    Diagram invocables work

--- src/main/java/org/nasdanika/demos/diagrams/proxy/SystemProcessor.java
+++ src/main/java/org/nasdanika/demos/diagrams/proxy/SystemProcessor.java
@@ -10,12 +10,16 @@ import org.nasdanika.common.Invocable;
 import org.nasdanika.common.ProgressMonitor;
 import org.nasdanika.drawio.Node;
 import org.nasdanika.graph.Element;
+import org.nasdanika.graph.processor.ConnectionProcessorConfig;
 import org.nasdanika.graph.processor.IncomingHandler;
+import org.nasdanika.graph.processor.NodeProcessorConfig;
 import org.nasdanika.graph.processor.ProcessorConfig;
 import org.nasdanika.graph.processor.ProcessorElement;
 import org.nasdanika.graph.processor.ProcessorInfo;
 
-
+/**
+ * This processor is not interacted with by the client code and therefore does not implement the processor type - {@link Invocable}. 
+ */
 public class SystemProcessor {
 	
 	private String amount;
@@ -24,7 +28,18 @@ public class SystemProcessor {
 	public void setElement(Node element) {
 		this.amount = element.getProperty("amount");
 	}
-	
+
+	/**
+	 * This is the constructor signature for graph processor classes which are to e instantiated by URIInvocableCapabilityFactory (org.nasdanika.capability.factories.URIInvocableCapabilityFactory).
+	 * Config may be of specific types {@link ProcessorConfig} - {@link NodeProcessorConfig} or {@link ConnectionProcessorConfig}.  
+	 * @param loader
+	 * @param loaderProgressMonitor
+	 * @param data
+	 * @param config
+	 * @param infoProvider
+	 * @param endpointWiringStageConsumer
+	 * @param wiringProgressMonitor
+	 */
 	public SystemProcessor(
 			Loader loader,
 			ProgressMonitor loaderProgressMonitor,
--- src/test/java/org/nasdanika/demos/diagrams/proxy/tests/TestDiagramExecution.java
+++ src/test/java/org/nasdanika/demos/diagrams/proxy/tests/TestDiagramExecution.java
@@ -1,12 +1,18 @@
 package org.nasdanika.demos.diagrams.proxy.tests;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
 import java.util.function.Function;
 
 import org.eclipse.emf.common.util.URI;
 import org.junit.jupiter.api.Test;
+import org.nasdanika.capability.CapabilityLoader;
+import org.nasdanika.capability.ServiceCapabilityFactory;
+import org.nasdanika.capability.requirements.DiagramRequirement;
+import org.nasdanika.capability.requirements.URIInvocableRequirement;
+import org.nasdanika.common.Invocable;
 import org.nasdanika.common.PrintStreamProgressMonitor;
 import org.nasdanika.common.ProgressMonitor;
 import org.nasdanika.drawio.Document;
@@ -36,4 +42,53 @@ public class TestDiagramExecution {
 		System.out.println(proxy.apply(33));
 	}	
 	
+	// implementation 'org.freemarker:freemarker:2.3.33'
+	
+	@Test
+	public void testDiagramRequirement() throws IOException {
+		CapabilityLoader capabilityLoader = new CapabilityLoader();
+		ProgressMonitor progressMonitor = new PrintStreamProgressMonitor();
+		URI specUri = URI.createFileURI(new File("diagram.drawio").getCanonicalPath());
+		DiagramRequirement requirement = new DiagramRequirement(
+				specUri, 
+				null,
+				null,
+				null, 
+				null, 
+				"processor", 
+				"bind", 
+				getClass().getClassLoader(), 
+				new Class<?>[] { java.util.function.Function.class });
+		
+		Function<String,Object> result = capabilityLoader.loadOne(requirement, progressMonitor);
+		System.out.println(result);
+		System.out.println(result.apply("YAML"));
+	}
+	
+	@Test
+	public void testYAMLSpec() throws IOException {
+		CapabilityLoader capabilityLoader = new CapabilityLoader();
+		ProgressMonitor progressMonitor = new PrintStreamProgressMonitor();
+		URI specUri = URI.createFileURI(new File("diagram-function.yml").getCanonicalPath());
+		Invocable invocable = capabilityLoader.loadOne(
+				ServiceCapabilityFactory.createRequirement(Invocable.class, null, new URIInvocableRequirement(specUri)),
+				progressMonitor);
+		Function<String,Object> result = invocable.invoke();
+		System.out.println(result);
+		System.out.println(result.apply("YAML"));
+	}
+	
+	@Test
+	public void testYAMLSpecInline() throws IOException {
+		CapabilityLoader capabilityLoader = new CapabilityLoader();
+		ProgressMonitor progressMonitor = new PrintStreamProgressMonitor();
+		URI specUri = URI.createFileURI(new File("diagram-function-inline.yml").getCanonicalPath());
+		Invocable invocable = capabilityLoader.loadOne(
+				ServiceCapabilityFactory.createRequirement(Invocable.class, null, new URIInvocableRequirement(specUri)),
+				progressMonitor);
+		Function<String,Object> result = invocable.invoke();
+		System.out.println(result);
+		System.out.println(result.apply("YAML"));
+	}
+		
 }
